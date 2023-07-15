0a40111ed6b33a5d1fd3dcf9f45f96da36432830
bloco314 <joseninja314@gmail.com>
2023-07-15 15:26:29 -0400

    tiny upgrades

--- static/move.js
+++ static/move.js
@@ -1,48 +1,27 @@
 const hero = document.querySelector('.hero')
+let heroRight = parseInt(hero.style.right) || 0
+let heroTop = parseInt(hero.style.top) || 0
 
-const move_left = () => {
-    if(parseInt(hero.style.right)<1020){
-        let x = parseInt(hero.style.right)+10
-        let novo =  parseInt(x)+'px'
-        hero.style.right = novo
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'ArrowRight' || event.code === 'KeyD') {
+    if (heroRight > 0) {
+      heroRight -= 10
+      hero.style.right = heroRight + 'px'
     }
-}
-
-const move_right = () => {
-    if(parseInt(hero.style.right)>0){
-        let x = parseInt(hero.style.right)-10
-        let novo =  parseInt(x)+'px'
-        hero.style.right = novo
+  } else if (event.code === 'ArrowLeft' || event.code === 'KeyA') {
+    if (heroRight < 1020) {
+      heroRight += 10
+      hero.style.right = heroRight + 'px'
     }
-}
-
-const move_up = () => {
-    if(parseInt(hero.style.top)>100){
-        let x = parseInt(hero.style.top)-10
-        let novo =  parseInt(x)+'px'
-        hero.style.top = novo
+  } else if (event.code === 'ArrowUp' || event.code === 'KeyW') {
+    if (heroTop > 100) {
+      heroTop -= 10
+      hero.style.top = heroTop + 'px'
     }
-}
-
-const move_down = () => {
-    if(parseInt(hero.style.top)<450){
-        let x = parseInt(hero.style.top)+10
-        let novo =  parseInt(x)+'px'
-        hero.style.top = novo
+  } else if (event.code === 'ArrowDown' || event.code === 'KeyS') {   
+    if (heroTop < 450) {
+      heroTop += 10
+      hero.style.top = heroTop + 'px'
     }
-}
-
-document.addEventListener('keydown',(event)=>{
-    if(event.code == 'ArrowRight' || event.code == 'KeyD'){
-        move_right()
-    }else
-    if(event.code == 'ArrowLeft' || event.code == 'KeyA'){
-        move_left()
-    }else
-    if(event.code == 'ArrowUp' || event.code == 'KeyW'){
-        move_up()
-    }else
-    if(event.code == 'ArrowDown' || event.code == 'KeyS'){
-        move_down()
-    }
-})
+  }
+});
--- static/obstaculos/generateCloudsV.js
+++ static/obstaculos/generateCloudsV.js
@@ -1,56 +1,53 @@
 function criarNuvemVertical() {
-  for (x = 0; x<dificuldade ; x++){
+  for (let x = 0; x < dificuldade; x++) {
     const imgElement = document.createElement('img');
     imgElement.src = "/static/assets/cloud_grey.png";
-    imgElement.classList.add('obstaculo-vertical')
-    var random = Math.floor(Math.random() * (1000));
-    imgElement.style.position = 'absolute'
-    imgElement.style.left = random + 'px'
-    const gameboard = document.querySelector('.game-board')
+    imgElement.classList.add('obstaculo-vertical');
+    const random = Math.floor(Math.random() * 1000);
+    imgElement.style.position = 'absolute';
+    imgElement.style.left = random + 'px';
+    const gameboard = document.querySelector('.game-board');
     gameboard.appendChild(imgElement);
   }
 }
 
-let ivuneravel = false
+let invulneravel = false;
 
-const isVerticalCloudOutOfBoard = () =>{
-    const imgElements = document.querySelectorAll('.obstaculo-vertical')
-    imgElements.forEach( img => {
-      if (parseInt(img.offsetTop) > 460){
-        const placar = document.querySelector('.placar')
-        let temp = parseInt(placar.innerHTML)+1
-        placar.innerHTML = temp
-        img.parentNode.removeChild(img)
+const isVerticalCloudOutOfBoard = () => {
+  const imgElements = document.querySelectorAll('.obstaculo-vertical');
+  imgElements.forEach((img) => {
+    if (img.offsetTop > 460) {
+      const placar = document.querySelector('.placar');
+      placar.innerHTML = parseInt(placar.innerHTML) + 1;
+      img.remove();
 
-        if( temp>=100 ){
-          window.location.href = nextLevel
-        }
-      }else{
-        if(Math.abs(img.offsetLeft - hero.offsetLeft)<30 
-        && Math.abs(img.offsetTop - hero.offsetTop)<30){
-          if (!ivuneravel){
-            ivuneravel = true
-            setTimeout(()=>{
-              ivuneravel = false
-            },2500)
-            if (vida == 3){
-              vida -= 1
-              const core = document.querySelector('.core-tree')
-              core.parentNode.removeChild(core)
-            }else
-            if (vida == 2){
-              vida -= 1
-              const core = document.querySelector('.core-two')
-              core.parentNode.removeChild(core)
-            }else{
-              window.location.href = '/gameover'
-            }
+      if (parseInt(placar.innerHTML) >= 100) {
+        window.location.href = nextLevel;
+      }
+    } else {
+      if (Math.abs(img.offsetLeft - hero.offsetLeft) < 30 && Math.abs(img.offsetTop - hero.offsetTop) < 30) {
+        if (!invulneravel) {
+          invulneravel = true;
+          setTimeout(() => {
+            invulneravel = false;
+          }, 2500);
+          if (vida === 3) {
+            vida -= 1;
+            const core = document.querySelector('.core-tree');
+            core.remove();
+          } else if (vida === 2) {
+            vida -= 1;
+            const core = document.querySelector('.core-two');
+            core.remove();
+          } else {
+            window.location.href = '/gameover';
           }
         }
       }
-    })
+    }
+  });
 }
 
-setInterval(isVerticalCloudOutOfBoard, 50)  
+setInterval(isVerticalCloudOutOfBoard, 50);
 setInterval(criarNuvemVertical, 3000);
 setInterval(criarNuvemVertical, 7000);
